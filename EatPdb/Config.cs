c93663939fc07a1c9576e7ffb6eec48e1f27cc32
CodeHz <CodeHz@outlook.com>
2020-02-11 14:18:31 +0800

    Add config feature

---
+++ EatPdb/Config.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+
+namespace EatPdb {
+#nullable enable
+    public class Config {
+        public interface IFilter {
+            public bool Filter(bool isCode, string name);
+        }
+        public interface IFilterData : IFilter { }
+        public class IsCodeFilter : IFilterData {
+            public IsCodeFilter(bool isCode) => IsCode = isCode;
+
+            public bool IsCode { get; set; }
+
+            public bool Filter(bool isCode, string name) => isCode == IsCode;
+        }
+        public class FullNameFilter : IFilterData {
+            public FullNameFilter(string name) => Name = name;
+
+            public string Name { get; set; } = "";
+
+            public bool Filter(bool isCode, string name) => name == Name;
+        }
+        public class PrefixFilter : IFilterData {
+            public PrefixFilter(string prefix) => Prefix = prefix;
+
+            public string Prefix { get; set; } = "";
+            public bool Filter(bool isCode, string name) => name.StartsWith(Prefix);
+        }
+        public class RegexFilter : IFilterData {
+            public RegexFilter(string pattern) => regex = new Regex(pattern, RegexOptions.Compiled);
+
+            public Regex regex;
+            public bool Filter(bool isCode, string name) => regex.IsMatch(name);
+        }
+        public class WhitelistFilter : IFilter {
+            public WhitelistFilter(IEnumerable<IFilterData>? filters) => Filters = filters;
+
+            public IEnumerable<IFilterData>? Filters { get; set; }
+            public bool Filter(bool isCode, string name) => Filters == null ? false : Filters.Select(filter => filter.Filter(isCode, name)).Contains(true);
+        }
+        public class BlacklistFilter : IFilter {
+            public BlacklistFilter(IEnumerable<IFilterData>? filters) => Filters = filters;
+
+            public IEnumerable<IFilterData>? Filters { get; set; }
+            public bool Filter(bool isCode, string name) => Filters == null ? true : !Filters.Select(filter => filter.Filter(isCode, name)).Contains(true);
+        }
+        public class YamlTypeConverter : IYamlTypeConverter {
+            public bool Accepts(Type type) => type == typeof(WhitelistFilter) || type == typeof(BlacklistFilter);
+            public object? ReadYaml(IParser parser, Type type) {
+                var data = new List<IFilterData>();
+                parser.Consume<SequenceStart>();
+                while (!parser.TryConsume<SequenceEnd>(out var _)) {
+                    if (parser.TryConsume<Scalar>(out var scalar)) {
+                        data.Add(new FullNameFilter(scalar.Value));
+                    } else if (parser.TryConsume<MappingStart>(out var _)) {
+                        if (parser.TryConsume<Scalar>(out var key)) {
+                            var value = parser.Consume<Scalar>();
+                            switch (key.Value) {
+                            case "is_code":
+                                data.Add(new IsCodeFilter(bool.Parse(value.Value)));
+                                break;
+                            case "full":
+                            case "name":
+                                data.Add(new FullNameFilter(value.Value));
+                                break;
+                            case "prefix":
+                                data.Add(new PrefixFilter(value.Value));
+                                break;
+                            case "regex":
+                            case "pattern":
+                                data.Add(new RegexFilter(value.Value));
+                                break;
+                            default:
+                                throw new NotImplementedException("Unknown key: " + value.Value);
+                            }
+                        }
+                        parser.Consume<MappingEnd>();
+                    } else {
+                        throw new NotImplementedException("Unknown list");
+                    }
+                }
+                if (type == typeof(WhitelistFilter)) {
+                    return new WhitelistFilter(data);
+                } else if (type == typeof(BlacklistFilter)) {
+                    return new BlacklistFilter(data);
+                }
+                throw new NotImplementedException();
+            }
+            public void WriteYaml(IEmitter emitter, object? value, Type type) => throw new NotImplementedException();
+        }
+        [Required, YamlMember(Alias = "in")]
+        public string InputFile { get; set; } = "";
+        [Required, YamlMember(Alias = "out")]
+        public string OutputFile { get; set; } = "";
+        [YamlMember(Alias = "pdb")]
+        public string PdbFile { get; set; } = "";
+        [YamlMember(Alias = "def")]
+        public string DefFile { get; set; } = "";
+        [YamlMember(Alias = "dll_name")]
+        public string DllName { get; set; } = "";
+        [YamlMember(Alias = "filter")]
+        public IFilter? Filter { get; set; }
+
+        public void ApplyDefault() {
+            if (PdbFile == "")
+                PdbFile = Path.Join(Path.GetDirectoryName(InputFile), Path.GetFileNameWithoutExtension(InputFile) + ".pdb");
+            if (DefFile == "")
+                DefFile = Path.Join(Path.GetDirectoryName(OutputFile), Path.GetFileNameWithoutExtension(OutputFile) + ".def");
+            if (DllName == "")
+                DllName = Path.GetFileName(OutputFile);
+        }
+    }
+#nullable restore
+}
--- EatPdb/Program.cs
+++ EatPdb/Program.cs
@@ -10,162 +10,186 @@ using System.Runtime.InteropServices;
 namespace EatPdb {
 
     internal class Program {
+        [Verb("rebuild", HelpText = "Rebuild PE file from PDB file (deprecated)")]
+        public class DeprecatedOptions {
 
-        public class Options {
-
-            [Option('i', "Input", Required = true, HelpText = "Input File")]
+            [Value(0, HelpText = "Input File")]
             public string InputFile { get; set; }
 
-            [Option('p', "Pdb", HelpText = "Pdb File")]
+            [Option("pdb", HelpText = "Pdb File")]
             public string PdbFile { get; set; }
 
-            [Option('o', "Ouput", Required = true, HelpText = "Output File")]
-            public string OuputFile { get; set; }
+            [Option('o', "out", Required = true, HelpText = "Output File")]
+            public string OutputFile { get; set; }
 
-            [Option("DllName", HelpText = "DllName")]
+            [Option("dll", HelpText = "DllName")]
             public string DllName { get; set; }
 
-            [Option('d', "Definition", HelpText = "Module-Definition file")]
+            [Option("def", HelpText = "Module-Definition file")]
             public string Definition { get; set; }
 
-            [Option('v', "Verbose", Default = false, HelpText = "Verbose Output")]
+            [Option('v', "berbose", Default = false, HelpText = "Verbose Output")]
             public bool Verbose { get; set; }
         }
 
-        private static void Main(string[] args) => Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(RealMain);
+        [Verb("exec", HelpText = "Using config")]
+        public class ConfigOptions {
+            [Value(0)]
+            public string ConfigFile { get; set; }
+            [Option('d', "debug", Default = false, HelpText = "Debug mode")]
+            public bool Debug { get; set; }
+            [Option('v', "verbose", Default = false, HelpText = "Verbose Output")]
+            public bool Verbose { get; set; }
+        }
+
+        private static void Main(string[] args) => Parser.Default.ParseArguments<DeprecatedOptions, ConfigOptions>(args)
+                .WithParsed<ConfigOptions>(ConfigMain)
+                .WithParsed<DeprecatedOptions>(DeprecatedMain);
 
         private static uint GetAlign(uint length, uint align = 512) => length % align == 0 ? length : (length / align + 1) * align;
 
-        private static bool FilterName(string name) {
-            if (name.StartsWith("_"))
-                return false;
-            if (name.StartsWith("?__"))
-                return false;
-            if (name.StartsWith("??_"))
-                return false;
-            if (name.StartsWith("??@"))
-                return false;
-            if (name.StartsWith("?$TSS"))
-                return false;
-            if (name.Contains("std@@Q"))
-                return false;
-            if (name.Contains("std@@U"))
-                return false;
-            return true;
+        private static void ConfigMain(ConfigOptions options) {
+            var deserializer = new YamlDotNet.Serialization.DeserializerBuilder()
+                .WithTagMapping("!whitelist", typeof(Config.WhitelistFilter))
+                .WithTagMapping("!blacklist", typeof(Config.BlacklistFilter))
+                .WithTypeConverter(new Config.YamlTypeConverter())
+                .Build();
+            var cfg = deserializer.Deserialize<Config>(File.ReadAllText(options.ConfigFile));
+            cfg.ApplyDefault();
+            if (options.Debug)
+                Console.WriteLine("{0}", ObjectDumper.Dump(cfg));
+            RealMain(cfg, options.Verbose);
         }
 
-        private static void RealMain(Options options) {
-            try {
-                if (options.PdbFile == null)
-                    options.PdbFile = Path.GetFileNameWithoutExtension(options.InputFile) + ".pdb";
-                if (options.DllName == null)
-                    options.DllName = Path.GetFileName(options.OuputFile);
-                if (options.Definition == null)
-                    options.Definition = Path.GetFileNameWithoutExtension(options.OuputFile) + ".def";
+        private static void DeprecatedMain(DeprecatedOptions options) {
+            var filter = new Config.BlacklistFilter(new List<Config.IFilterData> {
+                new Config.PrefixFilter("_"),
+                new Config.PrefixFilter("?__"),
+                new Config.PrefixFilter("??_"),
+                new Config.PrefixFilter("??@"),
+                new Config.PrefixFilter("?$TSS"),
+                new Config.RegexFilter("std@@[QU]"),
+            });
+            var cfg = new Config {
+                InputFile = options.InputFile,
+                OutputFile = options.OutputFile,
+                PdbFile = options.PdbFile,
+                DefFile = options.Definition,
+                DllName = options.DllName,
+                Filter = filter
+            };
+            cfg.ApplyDefault();
+            RealMain(cfg, options.Verbose);
+        }
 
-                // Copy input file to output file, so we do not need to touch input file again
-                File.Copy(options.InputFile, options.OuputFile, true);
-                using var file = File.Open(options.OuputFile, FileMode.Open, FileAccess.ReadWrite);
-                using var exp = File.CreateText(options.Definition);
-                using var action = new PEAction(file);
-                using var pdb = new PdbFileReader(options.PdbFile);
-                var symdb = new SymbolDatabase();
+        private static void RealMain(Config config, bool verbose) {
 
-                // Collect all symbols
-                foreach (var item in pdb.PublicSymbols.Where(item => FilterName(item.Name)))
-                    symdb.Add((uint) item.RelativeVirtualAddress, item.Name);
+            // Copy input file to output file, so we do not need to touch input file again
+            File.Copy(config.InputFile, config.OutputFile, true);
+            using var file = File.Open(config.OutputFile, FileMode.Open, FileAccess.ReadWrite);
+            using var exp = File.CreateText(config.DefFile);
+            using var action = new PEAction(file);
+            using var pdb = new PdbFileReader(config.PdbFile);
+            var symdb = new SymbolDatabase();
+            Console.WriteLine("filtering ({0})", pdb.PublicSymbols.Length);
 
-                // Exclude imported symbols
-                foreach (var sym in action.GetImportSymbols())
-                    if (symdb.RemoveName(sym) && options.Verbose)
-                        Console.WriteLine("Removed {0}", sym);
+            // Collect all symbols
+            foreach (var item in pdb.PublicSymbols.Where(item => config.Filter?.Filter(item.IsCode, item.Name) ?? true))
+                symdb.Add((uint) item.RelativeVirtualAddress, item.Name);
 
-                // Build cache
-                var cache = symdb.ToArray();
-                var sorted = symdb.Build();
+            Console.WriteLine("symbols collected: {0}", symdb.Count());
 
-                // Print exported symbols
-                exp.Write("LIBRARY " + options.DllName + "\n");
-                exp.Write("EXPORTS\n");
-                foreach (var (name, idx) in sorted)
-                    exp.Write(string.Format("\t{0}\n", name));
+            // Exclude imported symbols
+            foreach (var sym in action.GetImportSymbols())
+                if (symdb.RemoveName(sym) && verbose)
+                    Console.WriteLine("Removed {0}", sym);
 
-                // Calculate append length
-                uint length = 0;
-                var expdirlength = (uint)Marshal.SizeOf<ExportDir>();
-                var dllnamelength = (uint)(options.DllName.Length + 1);
-                var NumberOfFunctions = (uint) cache.Count();
-                var functionslength = NumberOfFunctions * 4;
-                var NumberOfNames = (uint) cache.Select(item => item.Value.Count).Aggregate(0, (a, b) => a + b);
-                var ordinalslength = NumberOfNames * 2;
-                var nameslength = NumberOfNames * 4;
-                var stringslength = (uint) sorted.Select(kv => kv.Key.Length + 1).Aggregate(0, (a, b) => a + b);
-                Console.WriteLine("NumberOfFunctions: {0}", NumberOfFunctions);
-                Console.WriteLine("NumberOfNames: {0}", NumberOfNames);
-                Console.WriteLine("Length Of ExportDir: {0}", expdirlength);
-                Console.WriteLine("Length Of DllName: {0}", dllnamelength);
-                Console.WriteLine("Length Of Functions: {0}", functionslength);
-                Console.WriteLine("Length Of Ordinals: {0}", ordinalslength);
-                Console.WriteLine("Length Of Names: {0}", nameslength);
-                Console.WriteLine("Length Of Strings: {0}", stringslength);
-                length = expdirlength + dllnamelength + functionslength + ordinalslength + nameslength + stringslength;
-                Console.WriteLine("Addition length: {0}", length);
+            Console.WriteLine("symbols filtered: {0}", symdb.Count());
 
-                // Start modify header
-                var VirtualEnd = action.GetEnding();
-                var OriginalSize = (uint)file.Length;
-                action.PatchNtHeader(GetAlign(length));
-                action.PatchDir(0, VirtualEnd, length);
-                {
-                    var header = new SectionHeader {};
-                    header.SetName(".hacked");
-                    header.Misc.VirtualSize = length;
-                    header.VirtualAddress = VirtualEnd;
-                    header.SizeOfRawData = GetAlign(length);
-                    header.PointerToRawData = OriginalSize;
-                    header.Characteristics = 0x40000040;
-                    action.AppendSection(header);
+            // Build cache
+            var cache = symdb.ToArray();
+            var sorted = symdb.Build();
+
+            // Print exported symbols
+            exp.Write("LIBRARY " + config.DllName + "\n");
+            exp.Write("EXPORTS\n");
+            foreach (var (name, idx) in sorted)
+                exp.Write(string.Format("\t{0}\n", name));
+
+            Console.WriteLine("starting rebuild PE file");
+
+            // Calculate append length
+            uint length = 0;
+            var expdirlength = (uint)Marshal.SizeOf<ExportDir>();
+            var dllnamelength = (uint)(config.DllName.Length + 1);
+            var NumberOfFunctions = (uint) cache.Count();
+            var functionslength = NumberOfFunctions * 4;
+            var NumberOfNames = (uint) cache.Select(item => item.Value.Count).Aggregate(0, (a, b) => a + b);
+            var ordinalslength = NumberOfNames * 2;
+            var nameslength = NumberOfNames * 4;
+            var stringslength = (uint) sorted.Select(kv => kv.Key.Length + 1).Aggregate(0, (a, b) => a + b);
+            Console.WriteLine("NumberOfFunctions: {0}", NumberOfFunctions);
+            Console.WriteLine("NumberOfNames: {0}", NumberOfNames);
+            Console.WriteLine("Length Of ExportDir: {0}", expdirlength);
+            Console.WriteLine("Length Of DllName: {0}", dllnamelength);
+            Console.WriteLine("Length Of Functions: {0}", functionslength);
+            Console.WriteLine("Length Of Ordinals: {0}", ordinalslength);
+            Console.WriteLine("Length Of Names: {0}", nameslength);
+            Console.WriteLine("Length Of Strings: {0}", stringslength);
+            length = expdirlength + dllnamelength + functionslength + ordinalslength + nameslength + stringslength;
+            Console.WriteLine("Addition length: {0}", length);
+
+            // Start modify header
+            var VirtualEnd = action.GetEnding();
+            var OriginalSize = (uint)file.Length;
+            action.PatchNtHeader(GetAlign(length));
+            action.PatchDir(0, VirtualEnd, length);
+            {
+                var header = new SectionHeader {};
+                header.SetName(".hacked");
+                header.Misc.VirtualSize = length;
+                header.VirtualAddress = VirtualEnd;
+                header.SizeOfRawData = GetAlign(length);
+                header.PointerToRawData = OriginalSize;
+                header.Characteristics = 0x40000040;
+                action.AppendSection(header);
+            }
+
+            // Write export table
+            file.SetLength(OriginalSize + GetAlign(length));
+            {
+                var expdir = new ExportDir {};
+                expdir.Name = VirtualEnd + expdirlength;
+                expdir.Base = 1;
+                expdir.NumberOfFunctions = NumberOfFunctions;
+                expdir.NumberOfNames = NumberOfNames;
+                expdir.AddressOfFunctions = VirtualEnd + expdirlength + dllnamelength;
+                expdir.AddressOfOrdinals = VirtualEnd + expdirlength + dllnamelength + functionslength;
+                expdir.AddressOfNames = VirtualEnd + expdirlength + dllnamelength + functionslength + ordinalslength + stringslength;
+                action.WriteExport(expdir);
+            }
+            action.Writer.WriteByteString(config.DllName);
+            foreach (var (key, _) in cache)
+                action.Writer.WriteStruct(new RVA { Value = key });
+            foreach (var (_, idx) in sorted)
+                action.Writer.WriteStruct(new Ordinal { Value = idx });
+            {
+                var strscache = new List<uint>();
+                var baseoff = (uint)file.Position;
+                var baserva = VirtualEnd + expdirlength + dllnamelength + functionslength + ordinalslength;
+                foreach (var (name, _) in sorted) {
+                    strscache.Add((uint) file.Position - baseoff + baserva);
+                    if (verbose)
+                        Console.WriteLine("{0:X8} -> {1:X8}", file.Position, file.Position - baseoff + baserva);
+                    action.Writer.WriteByteString(name);
                 }
 
-                // Write export table
-                file.SetLength(OriginalSize + GetAlign(length));
-                {
-                    var expdir = new ExportDir {};
-                    expdir.Name = VirtualEnd + expdirlength;
-                    expdir.Base = 1;
-                    expdir.NumberOfFunctions = NumberOfFunctions;
-                    expdir.NumberOfNames = NumberOfNames;
-                    expdir.AddressOfFunctions = VirtualEnd + expdirlength + dllnamelength;
-                    expdir.AddressOfOrdinals = VirtualEnd + expdirlength + dllnamelength + functionslength;
-                    expdir.AddressOfNames = VirtualEnd + expdirlength + dllnamelength + functionslength + ordinalslength + stringslength;
-                    action.WriteExport(expdir);
-                }
-                action.Writer.WriteByteString(options.DllName);
-                foreach (var (key, _) in cache)
-                    action.Writer.WriteStruct(new RVA { Value = key });
-                foreach (var (_, idx) in sorted)
-                    action.Writer.WriteStruct(new Ordinal { Value = idx });
-                {
-                    var strscache = new List<uint>();
-                    var baseoff = (uint)file.Position;
-                    var baserva = VirtualEnd + expdirlength + dllnamelength + functionslength + ordinalslength;
-                    foreach (var (name, _) in sorted) {
-                        strscache.Add((uint) file.Position - baseoff + baserva);
-                        if (options.Verbose)
-                            Console.WriteLine("{0:X8} -> {1:X8}", file.Position, file.Position - baseoff + baserva);
-                        action.Writer.WriteByteString(name);
-                    }
+                foreach (var add in strscache)
+                    action.Writer.WriteStruct(new RVA { Value = add });
 
-                    foreach (var add in strscache)
-                        action.Writer.WriteStruct(new RVA { Value = add });
-
-                    Console.WriteLine("VirtualEnd: {0} {0:X8}", VirtualEnd);
-                    Console.WriteLine("OriginalSize: {0} {0:X8}", OriginalSize);
-                    Console.WriteLine("RealAppend: {0} {0:X8}", file.Position - OriginalSize);
-                }
-            } catch (Exception e) {
-                Console.Error.WriteLine(e);
+                Console.WriteLine("VirtualEnd: {0} {0:X8}", VirtualEnd);
+                Console.WriteLine("OriginalSize: {0} {0:X8}", OriginalSize);
+                Console.WriteLine("RealAppend: {0} {0:X8}", file.Position - OriginalSize);
             }
         }
     }
